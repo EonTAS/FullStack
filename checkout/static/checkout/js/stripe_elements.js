746f01b8c6b14b93702099ff6ba7549df0530e08
Martin Oglesby <oglesby.mo@gmail.com>
2022-01-29 19:43:59 +0000

    Fixed small error in stripe js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,6 +2,7 @@ var stripePublicKey = $('#id_stripe_public_key').text().slice(1,-1)
 var clientSecret = $('#id_client_secret').text().slice(1,-1)
 
 var item = $('#id_project').text()
+
 var stripe = Stripe(stripePublicKey)
 
 var elements = stripe.elements();
@@ -46,57 +47,57 @@ form.addEventListener('submit', function(ev) {
     }
     var url = '/checkout/cache_checkout/'
     $.post(url, postData).done(function() {
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card,
-                billing_details: {
+        stripe.confirmCardPayment(clientSecret, 
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: $.trim(form.full_name.value),
+                        phone: $.trim(form.phone_number.value),
+                        email: $.trim(form.email.value),
+                        address: {
+                            line1:$.trim(form.street_address1.value),
+                            line2:$.trim(form.street_address2.value),
+                            city:$.trim(form.town_or_city.value),
+                            country:$.trim(form.country.value),
+                            state: $.trim(form.county.value)
+                        }
+                    },
+                },
+                shipping: {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
                     address: {
                         line1:$.trim(form.street_address1.value),
                         line2:$.trim(form.street_address2.value),
                         city:$.trim(form.town_or_city.value),
                         country:$.trim(form.country.value),
+                        postal_code: $.trim(form.postcode.value),
                         state: $.trim(form.county.value)
                     }
-                },
-            },
-            shipping: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                address: {
-                    line1:$.trim(form.street_address1.value),
-                    line2:$.trim(form.street_address2.value),
-                    city:$.trim(form.town_or_city.value),
-                    country:$.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value)
                 }
-            }
-        }).then(function(result) {
-            var errorDiv = document.getElementById('card-errors');
-            if (result.error) {
-                var html = `
-                    <span class="icon" role="alert">
-                        <i class="fas fa-times"></i>
-                    </span>
-                    <span>${result.error.message}</span>
-                `
-                $(errorDiv).html(html)
-                card.update({'disabled':false})
-                $('#submit-button').attr('disabled', false)
-    
-                $('#loading-overlay').fadeToggle(100);
-            } else {
-                errorDiv.textContent = ''
-                if (result.paymentIntent.status === 'succeeded') {
-                    form.submit()
+            }).then(function(result) {
+                var errorDiv = document.getElementById('card-errors');
+                if (result.error) {
+                    var html = `
+                        <span class="icon" role="alert">
+                            <i class="fas fa-times"></i>
+                        </span>
+                        <span>${result.error.message}</span>
+                    `
+                    $(errorDiv).html(html)
+                    card.update({'disabled':false})
+                    $('#submit-button').attr('disabled', false)
+                
+                    $('#loading-overlay').fadeToggle(100);
+                } else {
+                    errorDiv.textContent = ''
+                    if (result.paymentIntent.status === 'succeeded') {
+                        form.submit()
+                    }
                 }
-            }
+            })
         }).fail(function() {
             location.reload()
         })
     })
-
-})
