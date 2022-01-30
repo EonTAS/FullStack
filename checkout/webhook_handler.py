0ffd8716756aa0670c2ee493ff7cf1d0368bfdb8
Martin Oglesby <oglesby.mo@gmail.com>
2022-01-30 15:52:47 +0000

    Fixed webhook handler to not fail at adding objects

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from .models import Commission
+from .models import Commission, Project
+from django.contrib.auth.models import User
+
 import time
 class StripeWebhook_Handler:
     def __init__(self, request):
@@ -11,8 +13,8 @@ class StripeWebhook_Handler:
     def handle_payment_intent_succeeded(self, event):
         intent = event.data.object
         pid = intent.id
-        user = intent.metadata.user
-        item = intent.metadata.item
+        user = User.objects.get(id=intent.metadata.user)
+        item = Project.objects.get(id=intent.metadata.item)
 
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
@@ -29,12 +31,12 @@ class StripeWebhook_Handler:
                     full_name__iexact=shipping_details.name, 
                     email__iexact=shipping_details.name, 
                     phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.country, 
-                    postcode__iexact=shipping_details.postal_code, 
-                    town_or_city__iexact=shipping_details.city,
-                    street_address1__iexact=shipping_details.line1,
-                    street_address2__iexact=shipping_details.line2,
-                    county__iexact=shipping_details.state, 
+                    country__iexact=shipping_details.address.country, 
+                    postcode__iexact=shipping_details.address.postal_code, 
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state, 
                     commItem=item,
                     user=user,
                     stripe_pid=pid
@@ -54,23 +56,22 @@ class StripeWebhook_Handler:
                     full_name=shipping_details.name, 
                     email=shipping_details.name, 
                     phone_number=shipping_details.phone,
-                    country=shipping_details.country, 
-                    postcode=shipping_details.postal_code, 
-                    town_or_city=shipping_details.city,
-                    street_address1=shipping_details.line1,
-                    street_address2=shipping_details.line2,
-                    county=shipping_details.state,  
+                    country=shipping_details.address.country, 
+                    postcode=shipping_details.address.postal_code, 
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
                     commItem=item,
                     user=user,
                     stripe_pid=pid
                 )
             except Exception as e:
+                print(e)
                 if commission:
                     commission.delete()
                 return HttpResponse(content=f'Webhook received: {event["type"]} | ERROR {e}', status=500)
-
-
         return HttpResponse(content=f'Webhook received: {event["type"]} | SUCCESS order added to database', status=200) 
 
     def handle_payment_intent_failed(self, event):
-        return HttpResponse(content=f'Webhook received: {event["type"]}', status=200) 
+        return HttpResponse(content=f'Webhook received: {event["type"]}', status=200)
