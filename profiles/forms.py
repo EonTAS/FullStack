ec750a121100396f4e92e7bd23ee3b15885e1bbd
Martin Oglesby <oglesby.mo@gmail.com>
2022-01-30 22:19:00 +0000

    Added profile page form finally

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,10 +1,25 @@
 from .models import UserProfile
 from django import forms
 
+from django.contrib.auth.models import User
+
 class UserForm(forms.ModelForm):
+    sendEmailUpdates = forms.BooleanField(required=False, label="Send Email when funded projects get updated")
+
     class Meta:
-        model = UserProfile
-        fields = ('header', 'body')
-        labels = {
-        }
+        model = User
+        fields = ('username', 'first_name', 'last_name')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get("instance")
+        if instance is not None:
+            self.fields['sendEmailUpdates'].initial = instance.userprofile.email_updates
+
+    def save(self, commit=True):
+        instance = super(UserForm, self).save(commit=False)
+        instance.userprofile.email_updates = self.cleaned_data["sendEmailUpdates"]
         
+        if commit:
+            instance.save()
+        return instance
--- profiles/views.py
+++ profiles/views.py
@@ -2,30 +2,25 @@ from django.shortcuts import render
 from projects.models import Project, Category, Comment
 # Create your views here.
 from django.shortcuts import render, redirect, reverse, get_object_or_404, resolve_url
-
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required 
+
+from .forms import UserForm
 
 @login_required
 def profile(request):
     orders = request.user.commission_set.all()
-    
-    #if request.method == 'POST':
-    #    comment_form = CommentForm(data=request.POST)
-    #    if comment_form.is_valid():
-#
-    #        # Create Comment object but don't save to database yet
-    #        new_comment = comment_form.save(commit=False)
-    #        # Assign the current post to the comment
-    #        new_comment.item = project
-    #        new_comment.owner = request.user
-    #        # Save the comment to the database
-    #        new_comment.save()
-    #else:
-    #    comment_form = CommentForm()
+    if request.method == 'POST':
+        form = UserForm(data=request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User updated")
+    else:
+        form = UserForm(instance=request.user)
 
-    #print(request.user.fields)
     context = {
         "user": request.user,
-        "funded": orders
+        "funded": orders,
+        "form": form
     }
     return render(request, "profiles/profile.html", context)
