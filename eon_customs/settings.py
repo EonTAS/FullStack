6fac50cc9963f7a4a7097a3c19ec41ec27aef72e
Martin Oglesby <oglesby.mo@gmail.com>
2022-05-22 12:08:57 +0000

    more static file fixing

--- eon_customs/settings.py
+++ eon_customs/settings.py
@@ -155,8 +155,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticFiles'),)
 STATIC_ROOT ="static/"
 
 MEDIA_URL = "/media/"
